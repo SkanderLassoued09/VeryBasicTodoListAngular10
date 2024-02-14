716b964961df97cb8d03ef1d56dbec47332de812
@lassoued.skanderr <lassoued.skanderr@gmail.com>
2024-02-14 21:52:21 +0100

    1st

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent implements OnInit {
   id;
@@ -11,68 +11,61 @@ export class TodolistComponent implements OnInit {
   age;
   listofusers = [
     {
-      id:1,
-      name:"Skander",
-      age:24
+      id: 1,
+      name: 'Skander',
+      age: 24,
     },
     {
-      id:2,
-      name:"SkanderLass",
-      age:25
+      id: 2,
+      name: 'SkanderLass',
+      age: 25,
     },
     {
-      id:3,
-      name:"SkanderLassoued",
-      age:26
-    }
-  ]
+      id: 3,
+      name: 'SkanderLassoued',
+      age: 26,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-
-  submit(){
+  submit() {
     if (this.name != null && this.age != null) {
-
       let newlistuser = {
-        id:this.id? this.id:this.listofusers.length+1,
-        name:this.name,
-        age:this.age
+        id: this.id ? this.id : this.listofusers.length + 1,
+        name: this.name,
+        age: this.age,
       };
 
-      let found = this.listofusers.find(user => user.id == this.id);
+      let found = this.listofusers.find((user) => user.id == this.id);
 
       if (!found) {
         this.listofusers.push(newlistuser);
-      }else{
+      } else {
         this.listofusers[this.listofusers.indexOf(found)] = newlistuser;
       }
 
-      this.id = null ;
-      this.name = null ;
-      this.age = null ;
+      this.id = null;
+      this.name = null;
+      this.age = null;
     }
   }
 
-
-
-
-
-
-  delete(user){
+  delete(user) {
     let index = this.listofusers.indexOf(user);
-    console.log(index)
-    this.listofusers.splice(index , 1 );
-
+    console.log(index);
+    this.listofusers.splice(index, 1);
   }
 
-  edit(user){
-
+  edit(user) {
     this.id = user?.id;
     this.name = user?.name;
-    this.age = user?.age
+    this.age = user?.age;
   }
-
 }
+
+/**
+ * Hekllo nezih hh
+ */
